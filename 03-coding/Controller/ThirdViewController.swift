f3395a36cb530db02444ddd237a4238e2dd928db
Erik Felipe Gacitua Arenas <exegaci@MacBook-Pro-de-Erik-2.local>
2019-02-19 11:15:06 -0300

    Creación de tercera app con multi views

---
+++ 03-coding/Controller/ThirdViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ThirdViewController.swift
+//  03-coding
+//
+//  Created by Erik Felipe Gacitua Arenas on 2/19/19.
+//  Copyright © 2019 Erik Felipe Gacitua Arenas. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
