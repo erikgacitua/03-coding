ae6e395e278c2ab114397c1c4ecbfb37d08c2a49
Erik Felipe Gacitua Arenas <exegaci@MacBook-Pro-de-Erik-2.local>
2019-02-19 18:12:27 -0300

    Se han agrega nuevas funciones:
    *Se agrego una caja de texto
    *Se acciono el teclado
    *Se agrego acción al hacer "Enter" o click en "ir" el teclado se cierra
    *Se agrego una función que refleja el movimiento del slider y sincroniza los valores con el label de edad
    *Se agrega botón para validar edad

--- 03-coding/Controller/FirstViewController.swift
+++ 03-coding/Controller/FirstViewController.swift
@@ -7,14 +7,45 @@
 //
 
 import UIKit
+//En la clase del viewController agremas una (,) y agregamos "UITextFieldDelegate" para controlar que el teclado se cierre al momento de presionar la tecla enter o ir
+class FirstViewController: UIViewController, UITextFieldDelegate {
 
-class FirstViewController: UIViewController {
-
+    //Label Age
+    @IBOutlet weak var labelAge: UILabel!
+    @IBOutlet weak var sliderAge: UISlider!
+    
+    var userAge = -1
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.updateAgeLabel()
     }
-
-
+    //Comando que cierra el teclado
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+       
+        //Recuperamos el contenido del textfield si es que existe
+        if let theText = textField.text{
+                print(textField.text!)//El signo ! fuerza a poner siempre el texto ingresado (Se usa siempre y cuando el textFiel tiene un string)
+        }
+        return true
+    }
+    //Slider Move
+    @IBAction func sliderAgeMove(_ sender: UISlider) {
+        
+        self.updateAgeLabel()
+    }
+    //Función que muestra la edad
+    func updateAgeLabel(){
+        userAge = Int(self.sliderAge.value)
+        self.labelAge.text = "\(userAge)"
+    }
+    //Acción del botón
+    @IBAction func validateData(_ sender: UIButton) {
+        
+    }
+    
 }
 
