8bc2e6ec4bf89477b5aa9d6e851bb17f18c6b243
Erik Felipe Gacitua Arenas <exegaci@MacBook-Pro-de-Erik-2.local>
2019-02-19 10:44:43 -0300

    Initial Commit

---
+++ 03-coding/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  03-coding
+//
+//  Created by Erik Felipe Gacitua Arenas on 2/19/19.
+//  Copyright © 2019 Erik Felipe Gacitua Arenas. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
