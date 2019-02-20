73cf5a1c3003a6f11d580310d098a362a4046450
erikgacitua <erik.gacitua.a@gmail.com>
2019-02-19 21:48:59 -0300

    Se agrega nueva forma de realizar *Se agrega condiciones de cambio de fondo según if

--- 03-coding/Controller/FirstViewController.swift
+++ 03-coding/Controller/FirstViewController.swift
@@ -15,6 +15,7 @@ class FirstViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var sliderAge: UISlider!
     
     var userAge = -1
+    var userName = ""
     
     
     override func viewDidLoad() {
@@ -28,7 +29,10 @@ class FirstViewController: UIViewController, UITextFieldDelegate {
        
         //Recuperamos el contenido del textfield si es que existe
         if let theText = textField.text{
-                print(textField.text!)//El signo ! fuerza a poner siempre el texto ingresado (Se usa siempre y cuando el textFiel tiene un string)
+            //El signo ! fuerza a poner siempre el texto ingresado (Se usa siempre y cuando el textFiel tiene un string)
+            //print(textField.text!)
+            self.userName = theText
+            
         }
         return true
     }
@@ -44,7 +48,30 @@ class FirstViewController: UIViewController, UITextFieldDelegate {
     }
     //Acción del botón
     @IBAction func validateData(_ sender: UIButton) {
+        //Declaración de variable y se le da una condicón en una sola linea como un if (Técnica Boleana)
+        let shouldEnterTheParty = (userName == "Juan Gabriel") || (userAge >= 18)//&& condición para dos variables que cumplan 
         
+        if shouldEnterTheParty {
+            print("Bienvenido a la fiesta!")
+            self.view.backgroundColor = UIColor(displayP3Red: 49.0/255.0, green: 273.0/255.0, blue: 93.0/255.0, alpha: 0.7)
+        } else {
+            print("Lo siento, esta fiesta es privada, no tienes acceso")
+            self.view.backgroundColor = UIColor(displayP3Red: 250.0/255.0, green: 50.0/255.0, blue: 50.0/255.0, alpha: 0.8)
+        }
+        
+       /* if userName == "Juan Gabriel" {
+            print("Puedes pasar a la fiesta!")
+            //Este comando cambia el background de color según la condición
+            self.view.backgroundColor = UIColor(displayP3Red: 49.0/255.0, green: 273.0/255.0, blue: 93.0/255.0, alpha: 0.7)
+        }else{
+            if userAge >= 18{
+                print("Puedes pasar a la fiesta por ser mayor de edad")
+                self.view.backgroundColor = UIColor(displayP3Red: 50.0/255.0, green: 160.0/255.0, blue: 250.0/255.0, alpha: 0.7)
+            }else{
+                print("Lo siento, esta fiesta es privada, no tienes acceso")
+                self.view.backgroundColor = UIColor(displayP3Red: 250.0/255.0, green: 50.0/255.0, blue: 50.0/255.0, alpha: 0.8)
+            }
+        }*/
     }
     
 }
